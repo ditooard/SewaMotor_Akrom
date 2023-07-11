5e3cf8e56f5fda82c4b47ceba00c5f136c8a19ae
hendriwhyu <hendriwp123@gmail.com>
2023-07-11 19:47:07 +0700

    fixDashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\SewaController as AdminSewaController;
 use App\Http\Controllers\Admin\ValidasiController;
 use App\Http\Controllers\Customer\MemberController;
 use App\Http\Controllers\Customer\SewaController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -57,9 +58,7 @@ Route::post('/inputDataMotor', [KendaraanController::class, 'store'])->name('pos
 Route::put('/editDataMotor/{id}', [KendaraanController::class, 'update'])->name('editDataMotor');
 Route::delete('/hapusKendaraan/{id}', [KendaraanController::class,'destroy'])->name('hapusKendaraan');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
